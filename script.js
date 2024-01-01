8b783d6460cb151a7ec9e3265be6a5d109e94f37
M-Bayu-Aji <muhammadbayuajisutisnaputra@smkwikrama.sch.id>
2024-01-01 19:33:27 +0700

    Porotfolioclear

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-function scrollToElement(elementSelector, instance = 0) {
-    // Select all elements that match the given selector
-    const elements = document.querySelectorAll(elementSelector);
-    // Check if there are elements matching the selector and if the requested instance exists
-    if (elements.length > instance) {
-        // Scroll to the specified instance of the element
-        elements[instance].scrollIntoView({ behavior: 'smooth' });
-    }
-}
-
-const link1 = document.getElementById("link1");
-const link2 = document.getElementById("link2");
-const link3 = document.getElementById("link3");
-
-link1.addEventListener('click', () => {
-    scrollToElement('.about');
-});
-
-link2.addEventListener('click', () => {
-    // Scroll to the second element with "header" class
-    scrollToElement('.penghalang2');
-});
-
-link3.addEventListener('click', () => {
-    scrollToElement('.address');
-});
-
-// navbar
-
 const navbar = document.querySelector(".nav-container");
 
 window.addEventListener('scroll', () => {
@@ -34,3 +5,4 @@ window.addEventListener('scroll', () => {
     const windowposition = window.scrollY > 70;
     navbar.classList.toggle("scrolling-active", windowposition  ) 
 })
+
